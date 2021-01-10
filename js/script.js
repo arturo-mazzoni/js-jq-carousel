e0d7da114cb702d38efc0517e8b263539128ab80
arturo <arturomazzoni@gmail.com>
2021-01-10 14:51:37 +0100

    commit

--- js/script.js
+++ js/script.js
@@ -1,76 +1,73 @@
 $(document).ready(function(){
 
-  $('.next').click(pulsanteNext); //funzione di callback che parte dopo il click
-  $('.prev').click(pulsantePrev);
-
+ // cliccando cambio img verso destra
+  $( ".next" ).click(slideRight);
+ // cliccando cambio img verso sinistra
+  $( ".prev" ).click(slideLeft);
+ // cliccando su un pallino seleziono l'img corrispondente
   $('.nav i').click(function(){
     $('.nav i.active').removeClass('active');
     $(this).addClass('active');
-    $(".images img.active").removeClass("active"));
-    $("img").eq($(this).addClass("active"));
+    // $(".images img.active").removeClass("active"));
+    $("img").eq($(this).index()).addClass("active");
   });
-
 });
 
-function pulsanteNext(){
+function slideRight(){
 
-  // in questo modo non capisce più quale immagine è attiva
-  // $('.images img.active').removeClass('active');
-  // $('.images img.active').next().addClass('active');
+  var elementoImg = $('.images img.active');
+  var elementoCircle = $(".nav i.active");
 
-  var imgAttiva = $('.images img.active');
-  //è importante usare la variabile
+  elementoImg.removeClass("active");
+  elementoCircle.removeClass("active");
 
-  var pallinoAttivo = $('.nav i.active');
-
-  imgAttiva.removeClass('active');
-  imgAttiva.next().addClass('active');
-
-  pallinoAttivo.removeClass('active');
-  pallinoAttivo.next().addClass('active');
-
-  if(imgAttiva.hasClass('last')){
-    $('.images img.first').addClass('active');
-    $('.nav i.first').addClass('active');
+ // se sono all'img con classe last
+  if(elementoImg.hasClass("last")){
+    // allora seleziono la prima img
+    $(".images img.first").addClass("active");
+  } else {
+    //altrimenti seleziono quella dopo
+    elementoImg.next().addClass("active");
   }
 
+  // se sono al pallino con classe last
+  if(elementoCircle.hasClass("last")){
+    // allora seleziono il primo pallino
+    $(".nav i.first").addClass("active");
+  } else { //altrimenti seleziono quello dopo
+    elementoCircle.next().addClass("active");
+  }
 }
 
+function slideLeft(){
 
-function pulsantePrev(){
+  var elementoImg = $('.images img.active');
+  var elementoCircle = $(".nav i.active");
 
-  var imgAttiva = $('.images img.active');
-  var pallinoAttivo = $('.nav i.active');
+  elementoImg.removeClass("active");
+  elementoCircle.removeClass("active");
 
+ // se sono all'img con classe first
+  if(elementoImg.hasClass("first")){
+    // allora seleziono l'ultima img
+    $(".images img.last").addClass("active");
+  } else { //altrimenti seleziono quella prima
+    elementoImg.prev().addClass("active");
+  }
 
-  imgAttiva.removeClass("active");
-  imgAttiva.prev().addClass('active');
-
-  pallinoAttivo.removeClass('active');
-  pallinoAttivo.prev().addClass('active');
-
-
-
+  // se sono al pallino con classe first
+  if(elementoCircle.hasClass("first")){
+    // allora seleziono l'ultimo pallino
+    $(".nav i.last").addClass("active");
+  } else { //altrimenti seleziono quello prima
+    elementoCircle.prev().addClass("active");
+  }
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//
+$(document).keydown(function(e){
+  if (e.keyCode == 37){
+    slideLeft();
+  } else if (e.keyCode == 39){
+    slideRight();
+  }
+});
